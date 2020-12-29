6491e378cf1908a226c0eaa1e33150d6c7142258
Dylan Edwards <Dylan.edwards90@outlook.com>
2020-12-29 08:08:00 -0500

    Added templates for the different projects

---
+++ GuessingGameTemplate.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 11:32:49 2020
+
+@author: Dylan
+"""
+
+#Some of these items need to be uncommented to work. 
+
+#What library needs to be imported here to get a random number
+#import
+
+#Creates a random number between 1 and 10
+# number = 
+
+# Gets input from user/player
+player_name = input("Hello, what's your name?")
+
+#Start of guesses, where should this start at?
+#number_of_guesses = 
+
+
+print('Okay! '  + player_name + ' I am guessing a number between 1 and 10: ')
+
+
+#while number_of_guesses < 10:
+#     guess = int(input())
+#Create a variable to add to the number of guesses
+#     This is where your variable should go
+#
+# Finish the IF statement
+#     if guess :
+#         print("Guess is too low!")
+#
+# Finish the IF statement
+#     if guess :
+#         print("Guess is too high!")
+#
+# Finish the IF statement
+#     if guess : 
+#         break
+#
+# Finish the IF statement
+# if guess == number :
+#     print('Good guess! It took you ' + str(number_of_guesses) + ' tries to get it!')
+# else:
+#     print('Unlucky, the number was ' + str(number))
