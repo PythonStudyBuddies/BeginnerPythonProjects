391b91270c068d795959dbf6703cfc4df61847df
Dylan Edwards <Dylan.edwards90@outlook.com>
2020-12-31 08:38:32 -0500

    Creation of the RockPaperScissorsTemplate

--- RockPaperScissorsTemplate.py
+++ RockPaperScissorsTemplate.py
@@ -6,10 +6,12 @@ Created on Sat Dec 26 12:09:54 2020
 """
 #To make this more challenging, find a way to decrease human error.
 
-from random import randint
+#import a random integer from the library random
+#Your code here
 
 #create a list of play options
-t = ["Rock", "Paper", "Scissors"]
+#t should equal one of the three choices Rock, Paper, or Scissors
+#t = 
 
 #assign a random play to the computer
 computer = t[randint(0,2)]
@@ -17,27 +19,38 @@ computer = t[randint(0,2)]
 # set player to False
 player = False
 
+#This while loop will change to true when the player inputs their choice.
 while player == False:
-    #set player to true
-    player = input("Rock, Paper, Scissors?").capitalize().replace(" ", "")
-    if player == computer:
-        print("Tie! We both win!")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer,"covers", player)
-        else:
-            print("You Win!", player, "smashes", computer)
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer,"cuts", player)
-        else:
-            print("You Win!", player, "covers", computer)
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("You lose!", computer,"smashes", player)
-        else:
-            print("You Win!", player, "cut", computer)
-    else:
-        print("Wrong input value! Check your spelling!")
-    player = False
-    computer =t[randint(0,2)]
+    # #set player to true
+    # #Get the player to input Rock, Paper, or Scissors.
+    # player = 
+    #
+    # #Start of the if statements
+    # #Computer Ties the Player
+    # if player == computer:
+    #     print()
+    # #Player inputs Rock
+    # elif player == :
+    #     if computer == :
+    #         print()
+    #     else:
+    #         print()
+    # #Player inputs Paper
+    # elif player == :
+    #     if computer == :
+    #         print()
+    #     else:
+    #         print()
+    # #Player inputs Scissors
+    # elif player ==
+    #     if computer == 
+    #         print()
+    #     else:
+    #         print()
+    # #Else statement for if the person spells something wrong. Hint* this will be a print
+    # else:
+    #     #Your Code Here#
+    
+    # #Resets the player to False
+    # player = False
+    # computer =t[randint(0,2)]
